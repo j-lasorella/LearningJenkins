f7d46b4db978f0ea722d313696e58b298b3bc081
j-lasorella <jakob.lasorella@live.com>
2018-06-26 14:29:53 -0400

    Test example

---
+++ jUnitTest/src/main/java/jUnitTest/TestClass.java
@@ -0,0 +1,10 @@
+package jUnitTest;
+
+public class TestClass {
+	public static void main(String[] args) {
+		
+	}
+	public boolean test() {
+		return true;
+	}
+}
---
+++ jUnitTest/src/test/java/jUnitTest/TestClassTester.java
@@ -0,0 +1,11 @@
+package jUnitTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestClassTester {
+	@Test
+	public void test() {
+		assertEquals("Should return true", true, (new TestClass()).test());
+	}
+}
